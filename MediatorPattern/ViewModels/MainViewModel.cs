218695d480aecc236a08257793a82e660d874f74
Zaur Jafarov <zaurcfr@hotmail.com>
2021-06-08 10:22:06 +0400

    Add project files.

---
+++ MediatorPattern/ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using MediatorPattern.Command;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatorPattern.ViewModels
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public RelayCommand JoinCommand { get; set; }
+
+        public MainViewModel()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
